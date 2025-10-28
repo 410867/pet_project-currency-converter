033a7b5e2aad420f869247f59a955c3f2ba34502
410867 <oshchyp123@gmail.com>
2025-10-28 13:48:13 +0200

    Deleted .env.

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_RATES_URL,
+  baseURL: import.meta.env.VITE_RATES_URL ?? "https://bank.gov.ua/NBUStatService/v1/statdirectory",
   timeout: 10000,
 });
